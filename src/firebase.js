126dcfedd6db3f2df6a42e52a39e1499b1d8c162
Joyal S Palackel <joyal.palackel19@gmail.com>
2021-08-02 12:21:09 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBewovtpKkIZtgLfbhOR14sQ7yMTYDpEwk",
-    authDomain: "quest-23979.firebaseapp.com",
-    projectId: "quest-23979",
-    storageBucket: "quest-23979.appspot.com",
-    messagingSenderId: "677317184773",
-    appId: "1:677317184773:web:c98f605ccc8e964caefb6c"
+    apiKey: "###################",
+    authDomain: "###################",
+    projectId: "###################",
+    storageBucket: "###################",
+    messagingSenderId: "###################",
+    appId: "###################",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
